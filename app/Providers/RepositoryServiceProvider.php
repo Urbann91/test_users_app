ac3bd7bbd2687059013915003597a925205089f6
devstas <urbann91@gmail.com>
2022-03-07 16:08:19 +0700

    base repository + interfaces

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repository\Eloquent\BaseRepository;
+use App\Repository\Eloquent\UserRepository;
+use App\Repository\EloquentRepositoryInterface;
+use App\Repository\UserRepositoryInterface;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class RepositoryServiceProvider
+ */
+class RepositoryServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind(EloquentRepositoryInterface::class, BaseRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+    }
+}
